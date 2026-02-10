6f8071a9b64ec09106f81d1d5423d253913934f1
Nipun Theekshana <theekshana2@gmail.com>
2026-02-10 22:18:59 +0100

    chore: rename Supabase ANON_KEY to PUBLISHABLE_KEY in environment types

--- mobile/src/types/env.d.ts
+++ mobile/src/types/env.d.ts
@@ -1,7 +1,7 @@
 declare var process: {
   env: {
     EXPO_PUBLIC_SUPABASE_URL?: string;
-    EXPO_PUBLIC_SUPABASE_ANON_KEY?: string;
+    EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY?: string;
     [key: string]: string | undefined;
   };
 };
